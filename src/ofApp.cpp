5e3877f30c245962f2c5dd3eed371dc12d71e918
tespin <tristan.m.espinoza@gmail.com>
2023-01-20 23:52:52 -0800

    added poly lines back in for testing

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,27 +1,60 @@
 #include "ofApp.h"
 
 void ofApp::setup(){
-    cam1.setDeviceID(0);
-    cam1.setup(1280, 720);
-//    cam1.listDevices();
-    tracker1.setup();
     
-    cam2.setDeviceID(1);
-    cam2.setup(1280, 720);
-    tracker2.setup();
+    
+//    cam1.setDeviceID(0);
+//    cam1.setup(1280, 720);
+//    cam1.listDevices();
+//    tracker1.setup();
+//
+//    cam2.setDeviceID(1);
+//    cam2.setup(1280, 720);
+//    tracker2.setup();
+    
+    radius = 200;
+    
+    pos.x = ofGetWidth() / 2 - radius / 2;
+    pos.y = ofGetHeight() / 2;
+    
+    pos2.x = ofGetWidth() / 2 + radius / 2;
+    pos2.y = ofGetHeight() / 2;
+    
+    speed.x = 1.05;
+    speed.y = 1.35;
+    
+    blob.build(100);
+    blob2.build(100);
+    
+    blob.update(pos, radius, radius);
+    blob2.update(pos2, radius, radius);
+    
+    currentClipperType = ClipperLib::ctIntersection;
+    
+    clipper.addPolyline(blob.getPolyline(), ClipperLib::ptSubject);
+    clipper.addPolyline(blob2.getPolyline(), ClipperLib::ptClip);
+    
 }
 
 void ofApp::update(){
     currentTime = ofGetElapsedTimef();
 
-    updateTracker(cam1, tracker1, face1);
-    updateTracker(cam2, tracker2, face2);
+//    updateTracker(cam1, tracker1, face1);
+//    updateTracker(cam2, tracker2, face2);
 
-    if (face1.size() != 0 && face2.size() != 0) {
-        clips = getClips(face1, face2, ClipperLib::ctIntersection);
-    }
+//    if (face1.size() != 0 && face2.size() != 0) {
+//        clips = getClips(face1, face2, ClipperLib::ctIntersection);
+//    }
     
-    if (isIntersecting(face1, face2)) {
+    pos.x = ofGetMouseX();
+    pos.y = ofGetMouseY();
+    
+    blob.update(pos, radius, radius);
+    blob2.update(pos2, radius, radius);
+    
+    updateClipper();
+    
+    if (isIntersecting(blob, blob2)) {
         replayStarted = false;
         isRecording = true;
         if (!hasStarted) {
@@ -66,14 +99,24 @@ void ofApp::draw(){
     } else {
         cout << "drawing intersection" << endl;
         ofSetColor(0);
-        drawFace(face1);
-        drawFace(face2);
+        blob.draw();
+        blob2.draw();
+//        drawFace(face1);
+//        drawFace(face2);
 
         for (auto& line: clips) {
             ofSetColor(255, 0, 255);
             line.draw();
         }
     }
+}
+
+void ofApp::updateClipper() {
+    clips.clear();
+    clipper.Clear();
+    clipper.addPolyline(blob.getPolyline(), ClipperLib::ptSubject);
+    clipper.addPolyline(blob2.getPolyline(), ClipperLib::ptClip);
+    clips = clipper.getClipped(currentClipperType);
 }
 
 void ofApp::updateTracker(ofVideoGrabber& grabber, ofxFaceTracker2& tracker, ofPolyline& face) {
@@ -107,9 +150,16 @@ std::vector<ofPolyline> ofApp::getClips(ofPolyline& face1, ofPolyline& face2, Cl
     return clipper.getClipped(clipType);
 }
 
-bool ofApp::isIntersecting(ofPolyline& face1, ofPolyline& face2) {
-    for (glm::vec3& v : face1) {
-        if (face2.inside(v)) return true;
+bool ofApp::isIntersecting(Blob blob, Blob blob2) {
+    for (auto& v: blob.getPolyline() ) {
+        if (blob2.getPolyline().inside(v)) return true;
     }
     return false;
 }
+
+//bool ofApp::isIntersecting(ofPolyline& poly1, ofPolyline& poly2) {
+//    for (glm::vec3& v : poly1) {
+//        if (poly2.inside(v)) return true;
+//    }
+//    return false;
+//}
--- src/ofApp.h
+++ src/ofApp.h
@@ -14,20 +14,33 @@ class ofApp : public ofBaseApp{
 		void draw();
         
         void updateTracker(ofVideoGrabber& grabber, ofxFaceTracker2& tracker, ofPolyline& face);
+        void updateClipper();
         ofPolyline getFacePolyline(ofxFaceTracker2& tracker);
         void drawFace(ofPolyline& face);
         std::vector<ofPolyline> getClips(ofPolyline& face1, ofPolyline& face2, ClipperLib::ClipType clipType);
+        bool isIntersecting(Blob blob1, Blob blob2);
         bool isIntersecting(ofPolyline& face1, ofPolyline& face2);
     
         ofxFaceTracker2 tracker1;
         ofxFaceTracker2 tracker2;
         ofVideoGrabber cam1;
         ofVideoGrabber cam2;
+    
+        Blob blob;
+        Blob blob2;
+    
+        ofVec3f pos;
+        ofVec3f speed;
+        ofVec3f pos2;
+        
+        float yoff;
+        float radius;
 
         ofPolyline face1;
         ofPolyline face2;
     
         ofx::Clipper clipper;
+        ClipperLib::ClipType currentClipperType;
         std::vector<ofPolyline> clips;
         std::vector<std::pair<float, ofPolyline> > recordings;
         ofPolyline currentReplayContour;
