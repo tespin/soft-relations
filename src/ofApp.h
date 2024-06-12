fb71d4aa03bb78b02170581df6c65045057d7504
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2024-06-12 13:16:50 -0700

    fix: resetting src files

--- src/ofApp.h
+++ src/ofApp.h
@@ -6,42 +6,9 @@
 
 class ofApp : public ofBaseApp{
 
-	public:
-    
-        Blob blob;
-        Blob blob2;
-        std::vector<ofPolyline> clips;
-        std::vector<std::pair<float, ofPolyline> > recordings;
-        float currentTime;
-        float startTime;
-        float endTime;
-        float elapsedTime;
-        
-        bool hasStarted;
-        bool hasEnded;
-        bool isRecording;
-        bool replayStarted;
-    
-        ofx::Clipper clipper;
-        ClipperLib::ClipType currentClipperType;
-    
-        ofVec3f pos;
-        ofVec3f speed;
-    
-        ofVec3f pos2;
-
-        float yoff;
-        float radius;
-    
-        ofPolyline currentReplayContour;
-        float currentReplayTime;
-        float replayStartTime;
-    
+	public:    
         void setup();
 		void update();
 		void draw();
-    
-        void updateClipper();
-        bool isIntersecting();
 		
 };
